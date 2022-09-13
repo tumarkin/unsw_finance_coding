e4f019c414aa7839da2545ce31329ec77336c6b4
Robert <tumarkin@gmail.com>
2022-09-13 12:06:34 +1000

    pde: Dividend yield

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # unsw_finance_coding
 A collection of coding examples for UNSW Finance PhD and Honours students.
+
+## PDE
+
+An explicit Euler finite difference scheme for numerically solving 
+the Black-Scholes partial differential equation.
+
+Currently, this version supports only a European style option.
--- pde/main.py
+++ pde/main.py
@@ -2,7 +2,7 @@ from Pde import PdeSlice, GridParameters
 from fractions import Fraction
 from collections import namedtuple
 
-CallParameters = namedtuple('CallParameters', 'strike rf sigma')
+CallParameters = namedtuple('CallParameters', 'strike rf sigma dividend_yield')
 
 class BlackScholesCall(PdeSlice):
     def __init__(self, gp, cp):
@@ -24,11 +24,11 @@ class BlackScholesCall(PdeSlice):
         ix = grid_point.ix
         cp = self.call_parameters
 
-        return (cp.rf  * v) - (cp.rf * s  * self.ddx(ix)) - (0.5 * cp.sigma**2 * s**2 * self.d2dx2(ix))
+        return (cp.rf  * v) - ((cp.rf - cp.dividend_yield) * s  * self.ddx(ix)) - (0.5 * cp.sigma**2 * s**2 * self.d2dx2(ix))
         
 if __name__ == '__main__':
     gp = GridParameters(x_steps = 100, x_min = 1, x_max = 100, t = Fraction(1, 1), t_steps = 5000)
-    cp = CallParameters(strike = 50, rf = 0.10, sigma = 0.3)
+    cp = CallParameters(strike = 50, rf = 0.10, sigma = 0.3, dividend_yield = 0.05)
     pde = BlackScholesCall(gp, cp)
     pde.solve()
     for gp in pde.grid:
